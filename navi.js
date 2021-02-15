d2c78e9f0f2506a5f86bf6dd590937d7383013d4
ogulcancevik <cancevdev@gmail.com>
2021-02-15 19:28:36 +0300

    .

---
+++ navi.js
@@ -0,0 +1,10 @@
+var navi = document.querySelector(".navbar")
+
+const navOpen = () =>{
+    document.querySelector("#hamburger").classList.toggle("is-active")
+    navi.classList.toggle("nav-open")
+
+    navi.classList.toggle("nav-close")
+
+}
+
